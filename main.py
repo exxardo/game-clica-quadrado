a211940bbc332f11857d9113bebd98f5d6555512
exxardo <eduardoramirodematos@gmail.com>
2021-08-22 18:59:30 -0300

    Criação da janela e primeiros dois retangulos

--- main.py
+++ main.py
@@ -1,21 +1,43 @@
 import pygame
+import pygame.time
 
+#Iniciando o pygame:
 pygame.init()
 
-# em pixels
+# Dimensões da tela em pixels:
 largura_tela = 800
 altura_tela = 600
 
-tela = pygame.display.set_mode(largura_tela, altura_tela)
+# Configurando tamanho da tela:
+tela = pygame.display.set_mode((largura_tela, altura_tela))
+
+# Desenhando os retangulos
+    # Definição das cores:
+cor = (255, 255, 0)
+area = (100, 100, 30, 30)
+pygame.draw.rect(tela, cor, area)
+
+cor = (255, 0, 0)
+area = (200, 200, 30, 30)
+pygame.draw.rect(tela, cor, area)
+
+clock = pygame.time.Clock()
 
 terminou = False
 while not terminou:
     # Atualizar o desenho na tela:
     pygame.display.update()
 
+    # Configura 60 atualizações de tela por segundo:
+    clock.tick(60)
+
     # Checar os eventos do mouse:
-    for evento in pygame.event.get():
-        
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            terminou = True
 
+# Finaliza a tela:
+pygame.display.quit()
 
+# Finalizando o pygame:
 pygame.quit()
